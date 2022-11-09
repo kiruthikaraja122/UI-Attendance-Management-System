54e5d9fa309b1a8c9bc42fcd7b75202becf59408
kiruthikaraja122 <116725102+kiruthikaraja122@users.noreply.github.com>
2022-11-09 15:40:23 +0530

    Create Attendance Management System11.java
    updated

---
+++ Attendance Management System11.java
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance Management System</title>
+    <link rel="stylesheet"href="style18.css"/> 
+    <link rel="stylesheet"href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+</head>
+<body>
+
+    <div class ="container">
+
+        
+        <nav>
+
+            <ul>
+                <li><a href="#" class ="logo">
+                    <img src="image1/attendance.jpg">
+                    <span class ="nav-item">Admin</span>
+                </a>
+
+                </li>
+                <li><a href="#">
+                    <i class ='fas fa-menorah'></i>
+                   <span class="nav-item">Dashboard</span>
+
+                </a></li>
+                <li><a href="#">
+                    <i class ="fas fa-comment"></i>
+                    <span class="nav-item">Message</span>
+                </a></li>
+                <li><a href="#">
+                    <i class ="fas fa-database"></i>
+                    <span class="nav-item">Report</span>
+                </a></li>
+                <li><a href="another page.html">
+                
+                    <i class ="fas fa-chart-bar"></i>
+                    <span class="nav-item">Attendance</span>                        
+
+                </a></li>
+                <li><a href="#">
+                    <i class ="fas fa-cog"></i>
+                    <span class="nav-item">Setting</span>
+                </a></li>
+
+
+                <li><a href="#"class ="logout">
+                    <i class ="fas fa-sign-out-alt"></i>
+                    <span class="nav-item">Logout</span>
+                </a></li>
+             </ul>
+        </nav>
+            <div class="users">
+                <div class ="card">
+                    <img src="image1/Atten1.jpg">
+                      <h4>Attendance</h4>
+                </div>
+            </div>
+        </section>
+
+    </section>
+    </div>
+
+</body>
+</html>
